71c980886fa451d63e5cb776a80716d065ed2965
geracedillo <126698429+geracedillo@users.noreply.github.com>
2024-02-20 13:14:47 -0600

    Add files via upload

---
+++ Operaciones Equipo 3.R
@@ -0,0 +1,6 @@
+# Equipo 3 
+# Cecilia Rivas Ginzález A01284874
+# Gerardo Cedillo Corona A01704232
+# José Arturo Silva Flores A01198049
+# Luis David Sánchez Castillo A01275655
+# Nayeli Peña Martínez A01368516
